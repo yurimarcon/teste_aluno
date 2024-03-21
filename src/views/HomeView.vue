301c798c63a9f295831e2032374560e4eccd4c73
Yuri Marcon <yuri.erik.oliveira@gmail.com>
2024-03-21 12:44:41 -0300

    Testando edição

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,4 @@
-
+<!-- 
 <script setup>
 import {onMounted, reactive, ref, computed} from 'vue';
 import ListPokemons from '@/components/ListPokemons.vue';
@@ -86,5 +86,122 @@ const selectedPokemon = async (pokemon) => {
 
 .card-list .card-body{
     flex: none!important;
+}
+</style>
+
+
+ -->
+
+
+
+
+
+
+
+
+<script setup>
+import { onMounted, reactive, ref, computed } from "vue";
+import ListPokemons from "../components/ListPokemons.vue"
+import CardSelected from "../components/CardSelected.vue"
+
+let urlBaseSvg = ref("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/")
+let pokemons = reactive(ref());
+let searchPokemonField = ref("")
+let pokemonSelected = reactive(ref());
+let loading = ref(false)
+
+onMounted(()=>{
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+  .then(res => res.json())
+  .then(res => pokemons.value = res.results);
+})
+
+const pokemonsFiltered = computed(()=>{
+  if(pokemons.value && searchPokemonField.value){
+    return pokemons.value.filter(pokemon=>
+      pokemon.name.toLowerCase().includes(searchPokemonField.value.toLowerCase())
+    )
+  }
+  return pokemons.value;
+})
+
+const selectPokemon = async (pokemon) => {
+  loading.value = true;
+  await fetch(pokemon.url)
+  .then(res => res.json())
+  .then(res => pokemonSelected.value = res)
+  .catch(err => alert(err))
+  .finally(()=> loading.value = false)
+
+  console.log(pokemonSelected.value)
+  
+}
+
+</script>
+
+<template>
+  <main>
+    <div class="container text-body-secondary">
+      
+      <div class="row mt-4">
+        <div class="col-sm-12 col-md-6">
+          
+          <CardSelected
+          :name="pokemonSelected?.name"
+          :xp="pokemonSelected?.base_experience"
+          :height="pokemonSelected?.height"
+          :img="pokemonSelected?.sprites.other.dream_world.front_default"
+          :loading="loading"
+          />
+
+        </div>
+  
+        <div class="col-sm-12 col-md-6">
+          <div class="card card-list">
+            <div class="card-body row">
+              
+              <div class="mb-3">
+                <label 
+                hidden 
+                for="searchPokemonField" 
+                class="form-label">
+                Pesqiosar...
+                </label>
+
+                <input 
+                v-model="searchPokemonField"
+                type="text" 
+                class="form-control" 
+                id="searchPokemonField" 
+                placeholder="Pesquisar...">
+              </div>
+
+              <ListPokemons 
+              v-for="pokemon in pokemonsFiltered"
+              :key="pokemon.name"
+              :name="pokemon.name"
+              :urlBaseSvg="urlBaseSvg + pokemon.url.split('/')[6] + '.svg'"
+              @click="selectPokemon(pokemon)"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.card-list{
+  max-height: 75vh;
+  overflow-y: scroll;
+  overflow-x: hidden;
+}
+
+@media (max-width: 768px) {
+  .card-list{
+    max-height: 48vh;
+  }
 }
 </style>
